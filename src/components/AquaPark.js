274dc211e27fcad0d261cff94753364d812c1f13
KireMitrov <kire_val@yahoo.com>
2023-07-05 15:18:19 +0200

    Changed CSS

--- src/components/AquaPark.js
+++ src/components/AquaPark.js
@@ -15,14 +15,18 @@ function AquaPark () {
     
 
     return (
-        <div>
+        <div className="main-container">
             <h1>Aqua Park Monitoring</h1>
-            <label>Enter bracelet number</label><br/>
-            <input type="number" value={number} onChange={ (e) => SetNumber(e.target.value)}/><br></br>
-            <button onClick={() => setNotification(number, 15000)}>15s</button>
-            <button onClick={() => setNotification(number, 30000)}>30s</button>
-            <button onClick={() => setNotification(number, 45000)}>45s</button>
-            <button onClick={() => setNotification(number, 60000)}>1min</button>
+            <div className="inputs">
+                <label>Enter bracelet number</label>
+                <input type="number" value={number} onChange={ (e) => SetNumber(e.target.value)}/>
+            </div>
+            <div className="btns">
+                <button onClick={() => setNotification(number, 15000)}>15s</button>
+                <button onClick={() => setNotification(number, 30000)}>30s</button>
+                <button onClick={() => setNotification(number, 45000)}>45s</button>
+                <button onClick={() => setNotification(number, 60000)}>1min</button>
+            </div>
             {/* <button onClick={}>wave</button> */}
         </div>
     )
